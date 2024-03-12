3e71875061090a268f1e67bef7a37077f08fecf4
davidattip <138945499+davidattip@users.noreply.github.com>
2024-03-12 19:26:36 +0100

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+i//mport "@hotwired/turbo-rails"
+//import "controllers"
+//import 'bootstrap'
+//import 'C:\Users\david\Desktop\blog\app\assets\stylesheets\custom.css.scss';
+
+
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,31 @@
+/*
+// app/javascript/packs/application.js
+
+// Support pour Rails' csrf token dans les requêtes Ajax
+import Rails from "@rails/ujs"
+Rails.start()
+
+// Active le support pour Stimulus dans votre application (si vous l'utilisez)
+import "controllers"
+
+// Importe les styles de Bootstrap (si vous utilisez Bootstrap)
+import "bootstrap"
+
+// Importe tous les fichiers CSS dans le dossier 'stylesheets'
+import "C:\Users\david\Desktop\blog\app\assets\stylesheets\custom.css.scss"
+
+// Importe des fichiers JavaScript ou des librairies spécifiques à partir de node_modules
+// Exemple: import 'some-library'
+
+// Active Turbolinks pour naviguer plus rapidement entre les pages (si vous l'utilisez)
+import Turbolinks from "turbolinks"
+Turbolinks.start()
+
+// Active l'interaction avec les fenêtres modales de Bootstrap (si vous utilisez Bootstrap)
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
+// Votre code JavaScript personnalisé peut être placé ici
+*/
