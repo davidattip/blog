80c02d1f35757a08d827a717e592a0ead928f000
davidattip <138945499+davidattip@users.noreply.github.com>
2024-03-13 02:53:06 +0100

    ajustement

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-i//mport "@hotwired/turbo-rails"
+//import "@hotwired/turbo-rails"
 //import "controllers"
 //import 'bootstrap'
 //import 'C:\Users\david\Desktop\blog\app\assets\stylesheets\custom.css.scss';
